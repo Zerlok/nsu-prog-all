7425ccfe5a9c47c9059100b93c8a0c89d1ecba12
Danil Troshnev <denergytro@gmail.com>
2013-09-04 23:44:18 +0700

    task_3 date checker added

--- task_3/main.c
+++ task_3/main.c
@@ -1,19 +1,25 @@
 #include "mod.h"
-/*
-int check_input(int d, int m, int y) {
-	if (d > 31 || m > 12 || y > 9999) {
-		printf("Invalid date. Use this format: DD/MM/YYYY");
+
+int is_valid_date(int d, int m, int y) {
+	int months[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	int max_day = months[m - 1];
+	if (m == 2) {
+		max_day += is_leap_year(y);
+	}
+	if (d > max_day || m > 12 || y > 9999 || d < 1 || m < 1 || y < 0) {
+		printf("Invalid date:\n%d/%d/%d\n", d, m, y);
 		return 0;
-	} else if
-*/
+	}
+	return 1;
+}
 
 int main() {
 	int day1, month1, year1, day2, month2, year2;
 	scanf("%d/%d/%d", &day1, &month1, &year1);
 	scanf("%d/%d/%d", &day2, &month2, &year2);
 	printf("------\n");
-	//printf("%d\n", get_days(day1, month1, year1));
-	//printf("%d\n", get_days(day2, month2, year2));
-	printf("%d days\n", get_difference(day1, month1, year1, day2, month2, year2));
+	if (is_valid_date(day1, month1, year1) && is_valid_date(day2, month2, year2)) {
+		printf("%d days\n", get_difference(day1, month1, year1, day2, month2, year2));
+	}
 	return 0;
 }
