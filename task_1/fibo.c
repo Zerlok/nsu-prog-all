bf35c33c81a8721978ef8f67e87147d1c483dcaa
Danil Troshnev <denergytro@gmail.com>
2013-09-04 17:37:06 +0700

    task_1 solution updated

--- task_1/fibo.c
+++ task_1/fibo.c
@@ -4,6 +4,10 @@ int check_fibo(int n) {
 	if (n < 1) {
 		printf ("%d < 1\n", n);
 	}
+	if (n > 47) {
+		n = 47;
+		printf("Can print only first %d fibo\n", n);
+	}
 	if (n > 0) {
 		printf("0\n");
 	}
@@ -11,20 +15,18 @@ int check_fibo(int n) {
 		printf("1\n");
 	}
 	if (n > 2) {
-		return 1;
+		return n - 2;
 	} else return 0;
 }
 
 void print_fibo(int n) {
 	int a = 0, b = 1, x = a + b;
-	if (check_fibo(n)) {
-		n -= 2;
-		while (n > 0) {
-			printf("%d\n", x);
-			a = b;
-			b = x;
-			x = a + b;
-			n--;
-		}
+	n = check_fibo(n);
+	while (n > 0) {
+		printf("%d\n", x);
+		a = b;
+		b = x;
+		x = a + b;
+		n--;
 	}
 }
