676dff97f27009eb05f746a30b3c95f6fd3af646
Danil Troshnev <denergytro@gmail.com>
2013-09-04 16:50:21 +0700

    init

---
+++ fibo.c
@@ -0,0 +1,30 @@
+#include "mod.h"
+
+int check_fibo(int n) {
+	if (n < 1) {
+		printf ("%d < 1\n", n);
+	}
+	if (n > 0) {
+		printf("0\n");
+	}
+	if (n > 1) {
+		printf("1\n");
+	}
+	if (n > 2) {
+		return 1;
+	} else return 0;
+}
+
+
+
+void print_fibo(int n) {
+	int a = 0, b = 1, x = a + b;
+	if (check_fibo(n)) {
+		while (n > 0) {
+			printf("%d\n", a + b);
+			a = b;
+			b += a - b;
+			n--;
+		}
+	}
+}
---
+++ main.c
@@ -0,0 +1,8 @@
+#include "mod.h"
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	print_fibo(n);
+	return 0;
+}
