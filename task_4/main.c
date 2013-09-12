f5d5625874e2ff49bb8e45a2d7eb6b2604cf2996
Danil Troshnev <denergytro@gmail.com>
2013-09-12 07:54:59 +0700

    hgignore updated

--- task_4/main.c
+++ task_4/main.c
@@ -3,11 +3,11 @@
 int main() {
 	int cur_notation, new_notation = 0;
 	int *lst;
-	int i, len;
-	char numbers[LIMIT] = {0};
-	scanf("%s %d %d", &numbers, &cur_notation, &new_notation);
-	// lst, len = to_list_from_str(numbers);
-	printf("%d %d\n", to_list_from_str(numbers));
+	int i, len = 10;
+	char numbers[LIMIT];
+	scanf("%s %d %d", numbers, &cur_notation, &new_notation);
+	lst = to_list_from_str(numbers);
+	// printf("%d %d\n", to_list_from_str(numbers));
 	printf("%d\n", lst[0]);
 	for (i = 0; i < len; i++) {
 		printf("%d, ", lst[i]);
--- task_4/notations.c
+++ task_4/notations.c
@@ -1,11 +1,30 @@
 #include "mod.h"
 
+// int to_list_from_str(char num[LIMIT]) { //Returns list of digits from string
+// 	int i = 0;
+// 	int len = strlen(num);
+// 	int n;
+// 	int lst[len];
+// 	
+// 	while (i < len) {
+// 		n = toupper(num[i]);
+// 		if (n >= 'A' && n <= 'Z') {
+// 			lst[i] = n - 55;
+// 		} else if (n >= '0' && n <= '9') {
+// 			lst[i] = n - 48;
+// 		}
+// 		printf("%d - %d\n", i, lst[i]);
+// 		i++;
+// 	}
+// 	printf("len - %d\n", len);
+// 	return lst, len;
+// }
+
 int to_list_from_str(char num[LIMIT]) { //Returns list of digits from string
-	int i = 0;
+	int i, n = 0;
 	int len = strlen(num);
-	int n;
 	int lst[len];
-	
+
 	while (i < len) {
 		n = toupper(num[i]);
 		if (n >= 'A' && n <= 'Z') {
@@ -16,8 +35,8 @@ int to_list_from_str(char num[LIMIT]) { //Returns list of digits from string
 		printf("%d - %d\n", i, lst[i]);
 		i++;
 	}
-	printf("%d %d\n", *lst, len);
-	return lst, len;
+	printf("len - %d\n", len);
+	return lst;
 }
 
 int check_notation(int num[LIMIT], int cur_notation) {
