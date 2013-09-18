45598839938c53c9cbd8050c24235b2611ae688c
Danil Troshnev <denergytro@gmail.com>
2013-09-18 13:07:44 +0700

    Some function documentations added

--- task_4/main.c
+++ task_4/main.c
@@ -2,16 +2,10 @@
 
 int main() {
 	int cur_notation, new_notation = 0;
-	int *lst;
-	int i, len = 10;
-	char numbers[LIMIT];
+	char number[LIMIT];
 
-	scanf("%s %d %d", numbers, &cur_notation, &new_notation);
-	lst = to_list_from_str(numbers);
-	printf("%d\n", lst[0]);
-	// for (i = 0; i < len; i++) {
-	// 	printf("%d, ", lst[i]);
-	// }
+	scanf("%s %d %d", number, &cur_notation, &new_notation);
+	to_new_notation_from_curr(number, cur_notation, new_notation);
 	printf("\n");
 	return 0;
 }
--- task_4/notations.c
+++ task_4/notations.c
@@ -1,9 +1,24 @@
 #include "mod.h"
 
-int to_list_from_str(char num[LIMIT]) { //Returns list of digits from string
-	int i, n = 0;
+void print_lst(int *l, int len) { //Prints values and length of array.
+	int i;
+
+	printf("List: [%d", l[0]);
+	for (i = 1; i < len; i++) {
+		printf(", %d", l[i]);
+	}
+	printf("]\n");
+	printf("Len: %d\n", len);
+}
+
+int *to_list_from_str(char *num) {
+	/*
+	Gets string.
+	Returns address of digits array from string.
+	*/
+	int n, i = 0;
 	int len = strlen(num);
-	int lst[len];
+	static int lst[LIMIT];
 
 	while (i < len) {
 		n = toupper(num[i]);
@@ -12,31 +27,59 @@ int to_list_from_str(char num[LIMIT]) { //Returns list of digits from string
 		} else if (n >= '0' && n <= '9') {
 			lst[i] = n - 48;
 		}
-		printf("%d - %d\n", i, lst[i]);
-		i++;
+		// printf("%d - %d\n", i, lst[i]);
+		i += 1;
 	}
-	printf("len - %d\n", len);
+	// printf("len - %d\n", len);
 	return lst;
 }
 
-int check_notation(int num[LIMIT], int cur_notation) {
-	return 1;
+int check_notation(int *num, int len, int notation) {
+	/*
+	If numbers exsist in notation, returns "-1",
+	otherrwise - returns index of wrong number.
+
+	Input: *num, len, notation
+	*/
+	int i = 0;
+
+	while (i < len && num[i] < notation) {
+		i += 1;
+	}
+	if (i != len) {
+		return i;
+	} else {
+		return -1;
+	}
 }
 
-int to_notation_from_ten(char num[LIMIT], int notation) {
+int to_notation_from_ten(int *nums, int notation) {
 	int new_num = 0;
 	
 	return new_num;
 }
 
-int to_ten_from_notation(char num[LIMIT], int notation) {
+int to_ten_from_notation(int *nums, int notation) {
 	int ten_num = 0;
 	
 	return ten_num;
 }
 
-int to_new_from_curr(char num[LIMIT], int cur_notation, int new_notation) {
-	int new_num = 0;
+int to_new_notation_from_curr(char *num, int cur_notation, int new_notation) {
+	/*
 	
+	*/
+	int new_num = 0;
+	int *lst, len, indx;
+	
+	lst = to_list_from_str(number);
+	len = strlen(number);
+	indx = check_notation(lst, len, cur_notation);
+	if (indx == -1) {
+		print_lst(lst, len);
+		to_new_notation_from_curr(lst, len);
+	} else {
+		printf("%d\n", indx);
+	}
 	return new_num;
 }
