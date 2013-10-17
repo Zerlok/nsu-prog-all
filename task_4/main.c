28f2a6ef5a6996909a00a29100e207563a1013d5
Danil Troshnev <denergytro@gmail.com>
2013-10-17 18:55:23 +0700

    task 4 accepted

--- task_4/main.c
+++ task_4/main.c
@@ -1,11 +1,40 @@
 #include "mod.h"
 
-int main(int argc, char const *argv[]) {
-	int cur_notation, new_notation = 0;
-	char number[LIMIT];
+int main(int argc, char *argv[]) {
+	int cur_notation, new_notation;
+	char N[32], *number = N;
+	char *new_number;
 
-	scanf("%s %d %d", number, &cur_notation, &new_notation);
-	get_number_in_new_notation(number, cur_notation, new_notation);
-	printf("\n");
+	// scanf("%s %d %d", number, &cur_notation, &new_notation);
+	// printf("OK\n");
+	// number = '10';
+	// switch (argc) {
+	// 	case 1: scanf("%s %d %d", number, &cur_notation, &new_notation); printf("OK\n"); break;
+	// 	case 2: number = argv[2];
+	// 	case 3: cur_notation = atoi(argv[3]);
+	// 	case 4: new_notation = atoi(argv[4]);
+	// 	case 2: printf("%s\n", number);
+	// 	case 3: printf("%d\n", atoi(cur_notation));
+	// 	case 4: printf("%d\n", atoi(new_notation));
+	// }
+	if (argc == 1) {
+		// printf("You work with module notation.c\nType \"--help\" for help\n");
+		print_help();
+		scanf("%s %d %d", number, &cur_notation, &new_notation);
+	}
+	// if (argc == 2) {
+	// 	if (argv[1] == '--help') {
+	// 		print_help();
+	// 	}
+	// }
+	if (argc == 4) {
+		number = argv[1];
+		cur_notation = atoi(argv[2]);
+		new_notation = atoi(argv[3]);
+	}
+	new_number = get_number_in_new_notation(number, cur_notation, new_notation);
+	if (new_number) {
+		printf("%s\n", new_number);
+	}
 	return 0;
 }
--- task_4/notations.c
+++ task_4/notations.c
@@ -1,6 +1,16 @@
 #include "mod.h"
 
 void print_lst(int *l) {
+	/*
+	Prints array on screen.
+	
+	Input:
+		int *l - array of digits
+	Output:
+		None
+	Notice:
+		Array of digits must have value -1 (NUM_END).
+	*/
 	int i = 1;
 
 	printf("List: [%d", l[0]);
@@ -12,16 +22,39 @@ void print_lst(int *l) {
 	printf("Len: %d\n", i);
 }
 
-void print_error(int err_code, char *msg) {
-	switch (err_code) {
-		case ERR_UNKNOWN: printf("Unknown error:\n\t%s\n", msg);
-		case ERR_NOTATION: printf("Wrong notation: %s\n", msg);
-		case ERR_SYMBOL: printf("Wrong symbol: %s\n", msg);
-		case ERR_DIGIT: printf("Wrong digit: \"%s\"\n", msg);
+void reverse_lst(int *l) {
+	/*
+	Revers the array.
+	
+	Input:
+		int *l - array
+	Output:
+		None
+	*/
+	int x, i, len = get_len(l);
+
+	for(i = 0; 2 * i < len; i++) {
+		x = l[len - i - 1];
+		l[len - i - 1] = l[i];
+		l[i] = x;
 	}
 }
 
+void print_help() {
+	printf("Input format:\nN b1 b2\nWhere:\nN - number\nb1 - current basement\nb2 - new basement\n");
+}
+
 int get_len(int *lst) {
+	/*
+	Returns the length of numbers array.
+	
+	Input:
+		int *lst - array of digits
+	Output:
+		int - length
+	Notice:
+		Array of digits must have value -1 (NUM_END).
+	*/
 	int i = 0;
 
 	while (lst[i] != NUM_END) {
@@ -30,7 +63,62 @@ int get_len(int *lst) {
 	return i;
 }
 
+char to_char_from_int(int x) {
+	/*
+	Converts char symbol from ASCII code
+	and returns it or 0 value if symbol is not a word (A-Z || 0-9).
+	
+	Input:
+		int x - number of symbol in ASCII code
+	Output:
+		char - symbol
+		"0" - if symbol is not a word
+	*/
+	char chr;
+
+	if (x >= 10 && x <= 37) {
+		chr = x + WORD_DIFF;
+	} else if (x >= 0 && x <= 9) {
+		chr = x + DIGIT_DIFF;
+	} else {
+		printf("Unknown symbol - %d, %c\n", x, (char)x);
+		return 0;
+	}
+	return chr;
+}
+
+char *to_str_from_lst(int *lst) {
+	/*
+	Converts array of digits to array of symbols and returns the address of this array.
+	
+	Input:
+		int *lst - array of digits
+	Output:
+		char *str - array of symbols
+	Notice:
+		Array of digits must have value -1 (NUM_END).
+	*/
+	static char str[LIMIT];
+	int i = 0;
+
+	while (lst[i] != NUM_END) {
+		str[i] = to_char_from_int(lst[i]);
+		i += 1;
+	}
+	return str;
+}
+
 int to_digit_from_char(char ch) {
+	/*
+	Converts char symbol to ASCII code number
+	and returns it or "0" value if char symbol is not a word (A-z || 0-9).
+
+	Input:
+		char ch - symbol
+	Ouput:
+		int d - ASCII code of symbol
+		0 - if ch is not a word
+	*/
 	int d = toupper(ch);
 
 	if (d >= 'A' && d <= 'Z') {
@@ -38,13 +126,20 @@ int to_digit_from_char(char ch) {
 	} else if (d >= '0' && d <= '9') {
 		return d - DIGIT_DIFF;
 	} else {
-		// print_error(ERR_DIGIT, d);
 		printf("Unknown digit - %c\n", d);
 		return 0;
 	}
 }
 
 int *to_list_from_str(char *num) {
+	/*
+	Converts array of symbols to array of digits and returns the address of this array.
+	
+	Input:
+		char *str - array of symbols (string)
+	Output:
+		int *lst - array of digits
+	*/
 	int i = 0;
 	int len = strlen(num);
 	static int lst[LIMIT];
@@ -57,7 +152,19 @@ int *to_list_from_str(char *num) {
 	return lst;
 }
 
-int check_notation(int *num, int notation) {
+int check_number_in_notation(int *num, int notation) {
+	/*
+	Checks is number wrodec in correct notation.
+
+	Input:
+		int *num - array of digits
+		int notation - notation (basement) of array of digits (number)
+	Output:
+		int i - index of incorrect digit or
+		-1 - if all digits are correct
+	Notice:
+		Array of digits must have value -1 (NUM_END).
+	*/
 	int i = 0;
 
 	while (num[i] != NUM_END && num[i] < notation) {
@@ -69,34 +176,83 @@ int check_notation(int *num, int notation) {
 	return i;
 }
 
-int to_notation_from_ten(int *num, int notation) {
-	// int new_num = 0;
+int *to_notation_from_dec(int num, int notation) {
+	/*
+	Converts the number in Dec notation to number in given notation.
+
+	Input:
+		int num - decimal number
+		int notation - basement to convert number
+	Output:
+		int *new_num - the number array's address
+	*/
 	static int new_num[LIMIT];
-	int i;
+	int i = 0;
 
-
+	while (num) {
+		new_num[i] = num % notation;
+		num /= notation;
+		i += 1;
+	}
+	new_num[i] = -1;
+	reverse_lst(new_num);
 	return new_num;
 }
 
-int to_ten_from_notation(int *num, int notation) {
-	int ten_num = 0;
-	
-	return ten_num;
+unsigned long long to_dec_from_notation(int *num, int notation) {
+	/*
+	Converts the number in given notation to dec number.
+
+	Input:
+		int *num - array of digits (number)
+		int notation - basement of input number
+	Output:
+		unsigned long long - dec number
+	*/
+	unsigned long long dec_num = 0;
+	int i, len = get_len(num);
+
+	for (i = len - 1; i >= 0; i--) {
+		dec_num += num[i] * pow(notation, len - i - 1);
+	}
+	return dec_num;
 }
 
-int get_number_in_new_notation(char *num, int cur_notation, int new_notation) {
-	int *lst, *ten, *new_lst, indx;
+char *get_number_in_new_notation(char *num, int cur_notation, int new_notation) {
+	/*
+	Converts the number from current notation to new.
+	Returns number in new notation
+	or error code if it has a mistakes.
+
+	Input:
+		char *num - array of symbols (number in current notation)
+		int cur_notation - current notation of number
+		int new_notation - new notation of number
+	Output:
+		char *new_str - array of symbols (number in new notation)
+	*/
+	int *lst, *new_lst, indx;
+	unsigned long long dec;
 	
 	lst = to_list_from_str(num);
-	indx = check_notation(lst, cur_notation);
-	if (indx != NUM_END) {
-		// print_error(ERR_NOTATION, 'index');
-		printf("%d\n", indx);
-	} else if (cur_notation == new_notation) {
-		print_lst(lst);
-	} else {
-		ten = to_ten_from_notation(lst, cur_notation);
-		print_lst(lst);
+	indx = check_number_in_notation(lst, cur_notation);
+
+	if (cur_notation < MIN_NOTATION) {
+		printf("Wrong notation: %d\nWrite notation > 1.\n", cur_notation);
+		return NULL;
+	} else if (new_notation < MIN_NOTATION) {
+		printf("Wrong notation: %d\nWrite notation > 1.\n", new_notation);
+		return NULL;
+	} else if (indx != NUM_END) {
+		printf("Wrong symbol \"%c\" for %d notation.\n", num[indx], cur_notation);
+	} else if (cur_notation == new_notation || !lst[0]) {
+		return num;
+	} else if (lst[0]) {
+		dec = to_dec_from_notation(lst, cur_notation);
+		new_lst = to_notation_from_dec(dec, new_notation);
+		return to_str_from_lst(new_lst);
 	}
-	return new_num;
+	printf("OK\n");
+	return NULL;
+	printf("OK\n");
 }
