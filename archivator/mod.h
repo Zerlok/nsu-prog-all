348476425bf7abb3767df643e50ec10b7dbc5fa7
Danil Troshnev <denergytro@gmail.com>
2014-03-19 17:58:43 +0700

    simple debug added

--- archivator/mod.h
+++ archivator/mod.h
@@ -1,19 +1,40 @@
 /* MODULES */
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 
 /* APPLICATION VARS */
+
 #define ARC_NAME "Trarchive"
 #define VERSION "0.01"
 #define DEBUG
 
 
+/* DEBUG */
+#ifdef DEBUG
+
+void print_flag(char flag[]);
+void print_func_name(char name[]);
+void print_decor();
+void decorate(char str[]);
+void run_test();
+
+#define TESTMODE_FLAG "--test"
+#define TESTMODE_CODE 9000
+#define TESTMODE_ERROR -9000
+
+#endif
+
+
 /* FILE VARS*/
+
 #define ARC_FILE_TAG ".trar"
 
 
 /* FLAGS */
+
 #define FLAG_ADD "-a"
 #define FLAG_EXTR "-x"
 #define FLAG_LIST "-l"
@@ -22,25 +43,27 @@
 
 
 /* FUNCTION CODES < 100 */
+
 #define APPEND_CODE 10
 #define EXTRACT_CODE 20
 #define LIST_CODE 30
-#define TEST_CODE 40
+#define TESTSUM_CODE 40
 #define HELP_CODE 50
 
 
 /* ERRORS > 99 */
+
 #define ERROR_CODE 99
 #define ERR_UNKNOWN_FLAG 100
-#define ERR_NO_FLAG_ARG 200
-#define ERR_NOT_ARCH_FILE 300
+#define ERR_NO_FLAGS 200
+#define ERR_NO_FLAG_ARG 300
+#define ERR_NOT_ARCH_FILE 400
 
 
 /* FUNCTION IMAGES */
+
 void print_doc();
 void print_flag_help();
 void print_err(int err_code);
-void print_flag(char flag[]);
-void view_decor();
 
 int read_tags(int argc, char *argv[]);
--- archivator/trarchive.c
+++ archivator/trarchive.c
@@ -1,28 +1,113 @@
 #include "mod.h"
 
 
-/* ---------- ACCESSORY FUNCTIONS ---------- */
+/* ---------- DEBUG FUNCTIONS ---------- */
+#ifdef DEBUG
 
+void print_flag(char flag[])
+/*	
+Function simply prints a flag.
+
+Input:
+	flag - (*char) the flag.
+*/
+{
+	printf("flag : '%s'\n", flag);
+}
+
+
+void print_func_name(char name[])
+{
+	printf("\n-----RUN----->\t'%s' function:\n", name);
+}
+
+void print_end_func(char name[])
+{
+	printf("\n<-----END-----\t'%s' function:\n", name);
+}
+
+void print_decor()
+/*
+Prints decorating symbols in output.
+*/
+{
+	printf("+++++++++++++++++++++++++\n");
+}
+
+
+void decorate(char str[])
+/*
+Prints char symbols in decorating view.
+*/
+{
+	print_decor();
+	printf("\n");
+	printf("%s", str);
+	printf("\n");
+	print_decor();
+}
+
+
+void run_test()
+{
+	#ifdef DEBUG
+		print_func_name("run_test");
+	#endif
+
+
+	
+	#ifdef DEBUG
+		print_end_func("run_test");
+	#endif
+}
+
+
+#endif
+
+
+/* ---------- ACCESSORY FUNCTIONS ---------- */
 
 void print_doc()
 /*	Function shows documentation of archivator.	*/
 {
+	#ifdef DEBUG
+		print_func_name("print_doc");
+	#endif
+	
 	printf("%s - %s\n", ARC_NAME, VERSION);
 	printf("You're reading a documentation about %s\n", ARC_NAME);
 	printf("Archive tag is %s\n", ARC_FILE_TAG);
+	printf("Using: $ harc <flag> <file> <archive>\n");
+	printf("\n");
 	print_flag_help();
+	printf("\n");
+	printf("Examples:\n");
+	printf("\t$ harc -a file.txt arch.trar (append arch.trar with file.txt file)\n");
+	printf("\t$ harc -l arch.trar (show the list of arch.trar's files)\n");
+	
+	#ifdef DEBUG
+		print_end_func("print_doc");
+	#endif
 }
 
 
 void print_flag_help()
 /*	Function shows help about flags.	*/
 {
+	#ifdef DEBUG
+		print_func_name("print_flag_help");
+	#endif
+	
 	printf("You can use this flags:\n");
-	printf("   %s\t\t-\tto add file to archive. Usage: %s <file> <archive>\n", FLAG_ADD, FLAG_ADD);
-	printf("   %s\t\t-\tto extract file from archive. Usage: %s <file> <archive>\n", FLAG_EXTR, FLAG_EXTR);
-	printf("   %s\t\t-\tto get a list of files in archive. Usage: %s <archive>\n", FLAG_LIST, FLAG_LIST);
-	printf("   %s\t\t-\tto get a control sum of archive. Usage: %s <archive>\n", FLAG_TEST, FLAG_LIST);
-	printf("   %s\t-\tto get a documentation.\n", FLAG_HELP);
+	printf("   %s\t\t-\tto add file to archive. \t\t Usage: %s <file> <archive>\n", FLAG_ADD, FLAG_ADD);
+	printf("   %s\t\t-\tto extract file from archive. \t\t Usage: %s <file> <archive>\n", FLAG_EXTR, FLAG_EXTR);
+	printf("   %s\t\t-\tto get a list of files in archive. \t Usage: %s <archive>\n", FLAG_LIST, FLAG_LIST);
+	printf("   %s\t\t-\tto get a control sum of archive. \t Usage: %s <archive>\n", FLAG_TEST, FLAG_TEST);
+	printf("   %s\t-\tto show this help documentation.\n", FLAG_HELP);
+	
+	#ifdef DEBUG
+		print_end_func("print_flag_help");
+	#endif
 }
 
 
@@ -34,17 +119,27 @@ Input:
 	err_code - (int) the number of error code.
 */
 {
+	#ifdef DEBUG
+		print_func_name("print_err");
+	#endif
+
 	switch (err_code)
 	{
 		case ERR_NO_FLAG_ARG:
 			printf("Please write arguments after flags!\n");
 			print_flag_help();
 			break;
+		case ERR_NO_FLAGS:
+			printf("Please write flags!\n");
+			print_flag_help();
+			break;
 		case ERR_UNKNOWN_FLAG:
 			printf("Please read which flags can be used in help.\n");
+			print_flag_help();
 			break;
 		case ERR_NOT_ARCH_FILE:
-			printf("It should be a %s type of archive!\n", ARC_FILE_TAG);
+			printf("Unknown type of archive file!\n");
+			printf("It should be a %s type of archive.\n", ARC_FILE_TAG);
 			break;
 		default:
 		{
@@ -59,40 +154,10 @@ Input:
 			break;
 		}
 	}
-}
 
-
-void print_flag(char flag[])
-/*	
-Function simply prints a flag.
-
-Input:
-	flag - (*char) the flag.
-*/
-{
-	printf("'%s'\n", flag);
-}
-
-
-void view_decor()
-/*
-Prints decorating symbols in output.
-*/
-{
-	printf("+++++++++++++++++++++++++\n");
-}
-
-
-void decorate(char str[])
-/*
-Prints char symbols in decorating view.
-*/
-{
-	view_decor()
-	printf("\n");
-	printf("'%s'\n", str);
-	printf("\n");
-	view_decor()
+	#ifdef DEBUG
+		print_end_func("print_err");
+	#endif
 }
 
 
@@ -105,10 +170,19 @@ Input:
 	arg - (*char) the name of file.
 */
 {
+	#ifdef DEBUG
+		print_func_name("is_arch_file");
+	#endif
+	
 	if (!strstr(arg, ARC_FILE_TAG)) // If not archive file
 	{
 		return 1;
 	}
+
+	#ifdef DEBUG
+		print_end_func("is_arch_file");
+	#endif
+
 	return 0;
 }
 
@@ -123,6 +197,10 @@ Input:
 	argv - (**char) a list of arguments.
 */
 {
+	#ifdef DEBUG
+		print_func_name("read_flags");
+	#endif
+
 	int str_indx, arg_indx = 1;
 	
 	while (arg_indx < argc)
@@ -134,7 +212,6 @@ Input:
 			{
 				if (!is_arch_file(argv[arg_indx + 1]))
 				{
-					// printf("Going add '%s' file to '%s' archive\n", argv[arg_indx], argv[arg_indx + 1]);
 					return APPEND_CODE + arg_indx + 1;
 				}
 				else
@@ -154,7 +231,6 @@ Input:
 			{
 				if (!is_arch_file(argv[arg_indx + 1]))
 				{
-					// printf("Going extract '%s' file from '%s' archive\n", argv[arg_indx], argv[arg_indx + 1]);
 					return EXTRACT_CODE + arg_indx + 1;
 				}
 				else
@@ -173,7 +249,6 @@ Input:
 			{
 				if (!is_arch_file(argv[arg_indx + 1]))
 				{
-					// printf("Going show a list of files in '%s' archive\n", argv[arg_indx + 1]);
 					return LIST_CODE + arg_indx + 1;
 				}
 				else
@@ -192,8 +267,7 @@ Input:
 			{
 				if (!is_arch_file(argv[arg_indx + 1]))
 				{
-					// printf("Going count a test sum in '%s' archive\n", argv[arg_indx + 1]);
-					return TEST_CODE + arg_indx + 1;
+					return TESTSUM_CODE + arg_indx + 1;
 				}
 				else
 				{
@@ -207,38 +281,65 @@ Input:
 		}
 		else if (!strcmp(argv[arg_indx], FLAG_HELP))
 		{
-			printf("Going show a help\n");
 			return HELP_CODE;
 		}
+
+		#ifdef DEBUG
+			else if (!strcmp(argv[arg_indx], TESTMODE_FLAG))
+			{
+				return TESTMODE_CODE + arg_indx;
+			}
+		#endif
+
 		else if (strstr(argv[arg_indx], "-"))
 		{
-			print_flag(argv[arg_indx]);
+			// print_flag(argv[arg_indx]);
 			return ERR_UNKNOWN_FLAG;
 		}
 		else
 		{
 			printf("Last arg: '%s'\n", argv[arg_indx]);
 		}
+
 		arg_indx++;
 	}
-	return 0;
+	return ERR_NO_FLAGS + arg_indx;
 }
 
 
 /* ---------- ARCHIVE FUNCTIONS ---------- */
 
-
-void print_file()
+void print_file(char *file_name)
 {
+	#ifdef DEBUG
+		print_func_name("print_file");
+	#endif
+
 	FILE *file;
-	char string[20];
+	// char file_name[255];
+	char str[255];
 
-	file = fopen("_test/first.txt", "rb");
+	// file = fopen("_test/first.txt", "rb");
 
-	while fgets(string, sizeof(string), file)
+	// while fgets(string, sizeof(string), file)
+	// {
+	// 	printf("po\n");
+	// }
+
+	if((file = fopen(file_name, "r")) == NULL)
 	{
-		printf("po\n");
+		printf("Error while file opening!\n");
+		exit(1);
 	}
+
+	while((fscanf(file, "%s", str)) != EOF)
+	{
+		printf("%s\n", str);
+	}
+
+	#ifdef DEBUG
+		print_end_func("print_file");
+	#endif
 }
 
 
@@ -247,5 +348,70 @@ int *get_symbols_table(char *seq)
 ...
 */
 {
+	#ifdef DEBUG
+		print_func_name("get_symbols_table");
+	#endif
 
+
+
+	#ifdef DEBUG
+		print_end_func("get_symbols_table");
+	#endif
+
+	return NULL;
 }
+
+
+int *get_bin_tree(int *symbols_table)
+{
+	#ifdef DEBUG
+		print_func_name("get_bin_tree");
+	#endif
+
+
+
+	#ifdef DEBUG
+		print_end_func("get_bin_tree");
+	#endif
+
+	return NULL;
+}
+
+
+char *encode_symbols(int *tree)
+{
+	#ifdef DEBUG
+		print_func_name("encode_symbols");
+	#endif
+
+	// get_symbols_table(file);
+	// get_bin_tree(symbols_table);
+
+	// ...
+
+	#ifdef DEBUG
+		print_end_func("encode_symbols");
+	#endif
+
+	return NULL;
+}
+
+
+int *decode_symbols(char *symbols)
+{
+	#ifdef DEBUG
+		print_func_name("get_bin_tree");
+	#endif
+
+	// read_bin_tree(archive); ???
+	
+	// ...
+
+	#ifdef DEBUG
+		print_end_func("get_bin_tree");
+	#endif
+
+	return NULL;
+}
+
+
