e2434bf3951e0b9bd7b33a01af13fd46547818b5
Danil Troshnev <denergytro@gmail.com>
2015-05-29 02:56:12 +0600

    lab21 created

--- second_year/os/labs/lab2.c
+++ second_year/os/labs/lab2.c
@@ -1,4 +1,25 @@
-int main(int argc, char **argv)
+#define _GNU_SOURCE /* for tm_gmtoff and tm_zone */
+
+
+#include <stdio.h>
+#include <time.h>
+
+
+int main(int argc, char **argv, char **env)
 {
+	putenv("TZ=America/Los_Angeles");
+
+	time_t t = time(NULL);
+	struct tm lt;
+
+	localtime_r(&t, &lt);
+	printf("Current time in timezone '%s' is %02d:%02d:%02d.\n",
+		   lt.tm_zone,
+		   lt.tm_hour,
+		   lt.tm_min,
+		   lt.tm_sec
+	);
+
+
 	return 0;
 }
--- second_year/os/labs/lab21.c
+++ second_year/os/labs/lab21.c
@@ -1,4 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+
+int beep_cntr;
+
+
+void beep()
+{
+	printf("beep\n");
+
+	system("pacmd play-file /usr/share/sounds/KDE-Sys-App-Message.ogg alsa_output.pci-0000_00_1b.0.analog-stereo");
+	++beep_cntr;
+}
+
+
+void finish()
+{
+	printf("\nTOTAL BEEPS: %d\n", beep_cntr);
+	exit(1);
+}
+
+
+
+void scan_symbols()
+{
+	int c;
+	for (;;)
+	{
+		c = getchar();
+		
+		if (c == EOF)
+			break;
+		
+		printf("%d ", c);
+	}
+}
+
+
 int main(int argc, char **argv)
 {
+	beep_cntr = 0;
+
+	signal(SIGINT, finish);
+	signal(SIGQUIT, beep);
+
+	scan_symbols();
+
 	return 0;
 }
