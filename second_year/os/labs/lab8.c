fa7ba5ef876f5f0ff450df8cfac855c2c08fe89a
Danil Troshnev <denergytro@gmail.com>
2015-05-29 12:25:28 +0600

    lab8 copied

--- second_year/os/labs/lab8.c
+++ second_year/os/labs/lab8.c
@@ -1,4 +1,49 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+
+
+#define   MAXTRIES   5
+
+
 int main(int argc, char **argv)
 {
-	return 0;
+	int try = 0;
+	int fd = open("file.txt", O_RDONLY);
+	struct flock lock;
+
+	lock.l_type = F_RDLCK;
+	lock.l_whence = SEEK_SET;
+	lock.l_start = 0;
+	lock.l_len = 0; /* whole file address space */
+
+	while (fcntl(fd, F_SETLK, &lock) == -1)
+	{
+		if ((errno == EACCES) || (errno == EAGAIN))
+		{
+			++try;
+			if (try <= MAXTRIES)
+			{
+				sleep(1);
+				continue;
+			}
+
+			printf("%s busy -- try later\n",
+					argv[1]);
+		
+			exit(2);
+		}
+
+		perror(argv[1]);
+		exit(3);
+	}
+
+	system("nano");
+
+	lock.l_type = F_UNLCK;    /* unlock file */
+	fcntl(fd, F_SETLK, &lock);
+	close(fd);	return 0;
 }
