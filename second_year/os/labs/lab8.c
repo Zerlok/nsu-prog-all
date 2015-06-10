e77d8bc4e61bcef50ea17efea51c7684dbd36a18
Danil Troshnev <denergytro@gmail.com>
2015-06-11 01:03:00 +0600

    failed os labs

--- second_year/os/labs/lab8.c
+++ second_year/os/labs/lab8.c
@@ -1,49 +1,26 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/file.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <errno.h>
-
-
-#define   MAXTRIES   5
 
 
 int main(int argc, char **argv)
 {
 	int try = 0;
 	int fd = open("file.txt", O_RDONLY);
-	struct flock lock;
 
-	lock.l_type = F_RDLCK;
-	lock.l_whence = SEEK_SET;
-	lock.l_start = 0;
-	lock.l_len = 0; /* whole file address space */
-
-	while (fcntl(fd, F_SETLK, &lock) == -1)
+	if (flock(fd, LOCK_EX | LOCK_NB) < 0)
 	{
-		if ((errno == EACCES) || (errno == EAGAIN))
-		{
-			++try;
-			if (try <= MAXTRIES)
-			{
-				sleep(1);
-				continue;
-			}
-
-			printf("%s busy -- try later\n",
-					argv[1]);
-		
-			exit(2);
-		}
-
-		perror(argv[1]);
-		exit(3);
+		perror("flock");
+		exit(1);
 	}
 
-	system("nano");
+	// system("nano file.txt");
+	while (1);
 
-	lock.l_type = F_UNLCK;    /* unlock file */
-	fcntl(fd, F_SETLK, &lock);
-	close(fd);	return 0;
+	flock(fd, LOCK_UN);
+	close(fd);
+	return 0;
 }
