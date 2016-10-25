afabb6fe8bb6fdaf61ed298d5d10ea27372226d6
Danil Troshnev <denergytro@gmail.com>
2016-10-25 21:25:48 +0700

    screw shader updated with objPosition attribute

--- graphics/trogl/shaders/screwshader.cpp
+++ graphics/trogl/shaders/screwshader.cpp
@@ -10,6 +10,7 @@ const std::string ScrewShader::SRC = "\
 attribute vec4 position;\
 attribute vec4 color;\
 uniform float alpha;\
+uniform vec4 trogl_objPosition;\
 \
 mat3 rotationMatrix(vec3 axis, float angle)\
 {\
@@ -23,18 +24,12 @@ mat3 rotationMatrix(vec3 axis, float angle)\
 			 nc * axis.z * axis.x - axis.y * s,  nc * axis.y * axis.z + axis.x * s,  nc * axis.z * axis.z + c);\
 }\
 \
-int sign(vec3 v)\
-{\
- if (v.y > 0.0)\
-  return 1;\
- else\
-  return -1;\
-}\
-\
 void main() {\
  vec3 pos = position.xyz;\
  \
- pos = pos * rotationMatrix(vec3(0.0, 1.0, 0.0), alpha * sin(pos.y));\
+ pos -= trogl_objPosition.xyz;\
+ pos *= rotationMatrix(vec3(0.0, 1.0, 0.0), alpha * sin(pos.y));\
+ pos += trogl_objPosition.xyz;\
  gl_Position = gl_ModelViewProjectionMatrix * vec4(pos, 1.0);\
  gl_FrontColor = color;\
 }\
